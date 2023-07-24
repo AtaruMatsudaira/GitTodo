66d3bd39a7ad070ed5a37d9891beab0f7be1b025
mattun wsl <matsu222212@gmail.com>
2023-07-25 01:18:28 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using Cysharp.Diagnostics;
+using Zx;
+using static Zx.Env;
+
+var br = Environment.NewLine;
+
+var sb = new StringBuilder();
+
+await ConsoleApp.RunAsync(args, MainAsync);
+
+async Task<int> MainAsync(
+    [Option("path", "走査するディレクトリ")] string rootPath,
+    [Option("ex", "拡張子")] string extentions = "cs")
+{
+    await $"cd {rootPath}";
+
+    var csPaths = await $"find {rootPath} -type f -name '*.{extentions}'";
+    foreach (var csPath in csPaths.Split(br))
+    {
+        string grepResults;
+        try
+        {
+
+            grepResults = await $"grep -in '[^a-zA-Z]TODO[^a-zA-Z]' '{csPath}'";
+        }
+        catch (ProcessErrorException)
+        {
+            continue;
+        }
+
+        foreach (var grepResult in grepResults.Split(br))
+        {
+            var index = grepResult.IndexOf(':');
+            var lineNum = grepResult.Substring(0, index);
+            var content = grepResult.Substring(index + 1);
+
+            string blameInfos;
+
+            try
+            {
+                blameInfos = await $"git blame -L {lineNum},+1 {csPath} --porcelain";
+            }
+            catch (ProcessErrorException)
+            {
+                continue;
+            }
+            var author = blameInfos.Split(br).Where(info => info.StartsWith("author")).FirstOrDefault("");
+            sb.AppendLine($"{csPath}:{lineNum}:{author}");
+            sb.AppendLine(content);
+            sb.AppendLine();
+        }
+    }
+
+    log(sb.ToString(), ConsoleColor.Yellow);
+    
+
+    return 0;
+}
